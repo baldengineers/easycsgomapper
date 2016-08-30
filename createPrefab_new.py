3f24d41b24ad95f822e1731a3540348a278976c4
Jony <liu.z.jonathan@gmail.com>
2016-08-29 18:24:30 -0700

    Update createPrefab_new.py

--- createPrefab_new.py
+++ createPrefab_new.py
@@ -7,3 +7,132 @@ class Create():
 		#workshop_export | boolean | that determines whether the prefab will be zipped for export to the workshop
 		#indexLine |  | I have no fucking idea
 		#index |  | I have no idea about this either
+		
+		#self.compilelist is the outline of the prefab.py file
+		
+		self.compile_list = [
+  		"""import os
+import math
+def rotatePoint(centerPoint,point,angle):
+    angle = math.radians(angle)
+    temp_point = point[0]-centerPoint[0] , point[1]-centerPoint[1]
+    temp_point = ( temp_point[0]*math.cos(angle)-temp_point[1]*math.sin(angle) , temp_point[0]*math.sin(angle)+temp_point[1]*math.cos(angle))
+    temp_point = temp_point[0]+centerPoint[0] , temp_point[1]+centerPoint[1]
+    return temp_point
+def createTile(posx, posy, id_num, world_id_num, entity_num, placeholder_list, rotation, level):
+    
+    looplist = '1'
+    values=[]#Values are all of the lines of a prefab that have the vertex coords
+""",
+
+  "#INSERT_OPEN_FILE\n",
+
+  """
+    lines = f.readlines() #gathers each line of the prefab and puts numbers them
+""",
+
+  "#INSERT_PY_LIST\n",
+
+  "#INSERT_VAR_COUNT\n",
+
+  """
+    values = "".join(lines)#converting list to string
+    ogvalues = "".join(lines)
+    normal_list,axislist,negaxislist,vaxis,uaxis=[],['1 0 0 1','0 1 0 1','0 0 1 1'],['-1 0 0 1','0 -1 0 1','0 0 -1 1'],0,0
+    def evaluate(coords):
+        dist_x,dist_y,dist_z = abs(coords[0]),abs(coords[1]),abs(coords[2]),
+        if dist_x >= dist_y and dist_x >= dist_z:
+            return axislist[0]
+        if dist_y >= dist_z:
+            return axislist[1]
+        return axislist[2]
+    def get_normal(coord_list):
+        vector_a = (coord_list[1][0]-coord_list[0][0],coord_list[1][1]-coord_list[0][1],coord_list[1][2]-coord_list[0][2])
+        vector_b = (coord_list[2][0]-coord_list[0][0],coord_list[2][1]-coord_list[0][1],coord_list[2][2]-coord_list[0][2])
+        
+        normal = (vector_a[1]*vector_b[2]-vector_a[2]*vector_b[1],vector_a[2]*vector_b[0]-vector_a[0]*vector_b[2],vector_a[0]*vector_b[1]-vector_a[1]*vector_b[0])
+        return normal
+    
+    for normal_num in range(1,var_count+1,3):
+        normal_list=[]
+        for i in range(3):
+            normal_list.append([])
+            for var in ["x", "y", "z"]:
+                normal_list[i].append(eval(var+str(normal_num+i)))
+        coords = get_normal(normal_list)  
+        response = evaluate(coords)
+        if response == axislist[0]:
+            uaxis = axislist[1]
+        else:
+            uaxis = axislist[0]
+        if response == axislist[2]:
+            vaxis = negaxislist[1]
+        else:
+            vaxis = negaxislist[2]
+        values = values.replace('AXIS_REPLACE_U',uaxis,1)
+        values = values.replace('AXIS_REPLACE_V',vaxis,1)
+    
+    for i in range(ogvalues.count("world_idnum")):
+        values = values.replace('world_idnum', str(world_id_num), 1)
+        world_id_num += 1
+    
+    for var in ["x", "y", "z"]:
+        for count in range(1,var_count+1):
+            string = var + str(count)
+            string_var = str(eval(var + str(count)))
+            if var == "z":
+                values = values.replace(string + ")",string_var + ")") #we need to do this or else it will mess up on 2 digit numbers
+            else:
+                values = values.replace(string + " ",string_var + " ")
+    for i in range(ogvalues.count('id_num')):
+        values = values.replace('id_num', str(id_num), 1)
+        id_num = id_num+1
+        if "ROTATION_RIGHT" in values:
+            if rotation == 0:
+                values = values.replace("ROTATION_RIGHT","0 0 0",1)
+            elif rotation == 1:
+                values = values.replace("ROTATION_RIGHT","0 270 0",1)
+            elif rotation == 2:
+                values = values.replace("ROTATION_RIGHT","0 180 0",1)
+            elif rotation == 3:
+                values = values.replace("ROTATION_RIGHT","0 90 0",1)
+        if "ROTATION_UP" in values:
+            if rotation == 0:
+                values = values.replace("ROTATION_UP","0 90 0",1)
+            elif rotation == 1:
+                values = values.replace("ROTATION_UP","0 0 0",1)
+            elif rotation == 2:
+                values = values.replace("ROTATION_UP","0 270 0",1)
+            elif rotation == 3:
+                values = values.replace("ROTATION_UP","0 180 0",1)
+        if "ROTATION_LEFT" in values:
+            if rotation == 0:
+                values = values.replace("ROTATION_LEFT","0 180 0",1)
+            elif rotation == 1:
+                values = values.replace("ROTATION_LEFT","0 90 0",1)
+            elif rotation == 2:
+                values = values.replace("ROTATION_LEFT","0 0 0",1)
+            elif rotation == 3:
+                values = values.replace("ROTATION_LEFT","0 270 0",1)
+        if "ROTATION_DOWN" in values:
+            if rotation == 0:
+                values = values.replace("ROTATION_DOWN","0 270 0",1)
+            elif rotation == 1:
+                values = values.replace("ROTATION_DOWN","0 180 0",1)
+            elif rotation == 2:
+                values = values.replace("ROTATION_DOWN","0 90 0",1)
+            elif rotation == 3:
+                values = values.replace("ROTATION_DOWN","0 0 0",1)
+    values = values.replace('"[0 0 0 1] 0.25"','"[1 1 1 1] 0.25"')
+    values = values.replace('"[0 0 1 0] 0.25"','"[1 1 1 1] 0.25"')
+    values = values.replace('"[0 1 0 0] 0.25"','"[1 1 1 1] 0.25"')       
+    values = values.replace('"[1 0 0 0] 0.25"','"[1 1 1 1] 0.25"')
+    	""",
+	
+		"#INSERT_ENT_CODE\n",]
+	
+		with open(vmf_file, "r") as f:
+			vmf_data = f.readlines()
+			
+			for line in vmf_data:
+				
