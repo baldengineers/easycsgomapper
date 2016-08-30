c8e49e28354521aff149268b07fb5400a58d52e7
Jony <liu.z.jonathan@gmail.com>
2016-08-29 22:53:05 -0700

    started working on the new and improved createPrefab!!!!!

--- createPrefab_new.py
+++ createPrefab_new.py
@@ -1,17 +1,17 @@
 class Create():
-	def __init__(self, vmf_file, prefab_name, prefab_text, prefab_icon, workshop_export,indexLine,index):
-		#vmf_file | string | contains the filepath of the vmf file of the prefab
-		#prefab_name | string | is the filename of the prefab file being created
-		#prefab_text | string | is the name of the prefab as it will appear in the main application window
-		#prefab_icon | string | is the filepath of the icon of the prefab as it will appear in the main application window
-		#workshop_export | boolean | that determines whether the prefab will be zipped for export to the workshop
-		#indexLine |  | I have no fucking idea
-		#index |  | I have no idea about this either
-		
-		#self.compilelist is the outline of the prefab.py file
-		
-		self.compile_list = [
-  		"""import os
+    def __init__(self, vmf_file, prefab_name, prefab_text, prefab_icon, workshop_export,indexLine,index):
+        #vmf_file | string | contains the filepath of the vmf file of the prefab
+        #prefab_name | string | is the filename of the prefab file being created
+        #prefab_text | string | is the name of the prefab as it will appear in the main application window
+        #prefab_icon | string | is the filepath of the icon of the prefab as it will appear in the main application window
+        #workshop_export | boolean | that determines whether the prefab will be zipped for export to the workshop
+        #indexLine |  | I have no fucking idea
+        #index |  | I have no idea about this either
+
+        #self.compilelist is the outline of the prefab.py file
+
+        self.compile_list = [
+        """import os
 import math
 def rotatePoint(centerPoint,point,angle):
     angle = math.radians(angle)
@@ -20,7 +20,7 @@ def rotatePoint(centerPoint,point,angle):
     temp_point = temp_point[0]+centerPoint[0] , temp_point[1]+centerPoint[1]
     return temp_point
 def createTile(posx, posy, id_num, world_id_num, entity_num, placeholder_list, rotation, level):
-    
+
     looplist = '1'
     values=[]#Values are all of the lines of a prefab that have the vertex coords
 """,
@@ -49,17 +49,17 @@ def createTile(posx, posy, id_num, world_id_num, entity_num, placeholder_list, r
     def get_normal(coord_list):
         vector_a = (coord_list[1][0]-coord_list[0][0],coord_list[1][1]-coord_list[0][1],coord_list[1][2]-coord_list[0][2])
         vector_b = (coord_list[2][0]-coord_list[0][0],coord_list[2][1]-coord_list[0][1],coord_list[2][2]-coord_list[0][2])
-        
+
         normal = (vector_a[1]*vector_b[2]-vector_a[2]*vector_b[1],vector_a[2]*vector_b[0]-vector_a[0]*vector_b[2],vector_a[0]*vector_b[1]-vector_a[1]*vector_b[0])
         return normal
-    
+
     for normal_num in range(1,var_count+1,3):
         normal_list=[]
         for i in range(3):
             normal_list.append([])
             for var in ["x", "y", "z"]:
                 normal_list[i].append(eval(var+str(normal_num+i)))
-        coords = get_normal(normal_list)  
+        coords = get_normal(normal_list)
         response = evaluate(coords)
         if response == axislist[0]:
             uaxis = axislist[1]
@@ -71,11 +71,11 @@ def createTile(posx, posy, id_num, world_id_num, entity_num, placeholder_list, r
             vaxis = negaxislist[2]
         values = values.replace('AXIS_REPLACE_U',uaxis,1)
         values = values.replace('AXIS_REPLACE_V',vaxis,1)
-    
+
     for i in range(ogvalues.count("world_idnum")):
         values = values.replace('world_idnum', str(world_id_num), 1)
         world_id_num += 1
-    
+
     for var in ["x", "y", "z"]:
         for count in range(1,var_count+1):
             string = var + str(count)
@@ -125,15 +125,44 @@ def createTile(posx, posy, id_num, world_id_num, entity_num, placeholder_list, r
                 values = values.replace("ROTATION_DOWN","0 0 0",1)
     values = values.replace('"[0 0 0 1] 0.25"','"[1 1 1 1] 0.25"')
     values = values.replace('"[0 0 1 0] 0.25"','"[1 1 1 1] 0.25"')
-    values = values.replace('"[0 1 0 0] 0.25"','"[1 1 1 1] 0.25"')       
+    values = values.replace('"[0 1 0 0] 0.25"','"[1 1 1 1] 0.25"')
     values = values.replace('"[1 0 0 0] 0.25"','"[1 1 1 1] 0.25"')
-    	""",
-	
-		"#INSERT_ENT_CODE\n",]
-	
-		with open(vmf_file, "r") as f:
-			vmf_data = f.readlines()
-			
-			for line in vmf_data:
-			
-				
+        """,
+
+        "#INSERT_ENT_CODE\n",]
+
+        block_type = "" #block_type contains the current block of code the program is currently looking at. A block of code is determined by the two brackets {}/()/[] surrounding it.
+        var_num = 1 #var_num is the number that appears after the variable. ex. (x1 y1 z1) (x2 y2 z2) (x3 y3 z3)
+        
+        with open(vmf_file, "r") as f:
+            vmf_data = f.readlines()
+            header = False
+
+            for index, line in enumerate(vmf_data):
+                if "solid" in line:
+                    if header:
+                        header = False
+                        del vmf_data[:vmf_data.index(line)-1] #potential error here because still referencing original list in the for loop, and elsewhere in the program
+                    block_type = "solid"
+                elif "side" in line:
+                    block_type = "side"
+                elif "id" in line:
+                    if block_type == "solid":
+                        id_var = "world_idnum"
+                    elif block_type == "side":
+                        id_var = "id_num"
+                    vmf_data[index] = vmf_data[:vmf_data[index].index("id")+5] + id_var + "\"" #This line does this: "id" " + id_var + "
+                elif "(" in line:
+                    if block_type == "side":
+                        for char in line:
+                            if char == "(":
+                                num = ["","",""] #num is for the numbers in the parenthesis in the points for the plane
+                                ind = 0
+                                block_type = "()"
+
+                            if block_type = "()":
+                                if not char == " ":
+                                    num[ind] += char
+                                else:
+                                    ind += 1
+                    
