a402c24fb763414cf5e5428a1025c4a95bd2f108
Jony <liu.z.jonathan@gmail.com>
2016-08-29 17:58:56 -0700

    some useful commentary

--- createPrefab_new.py
+++ createPrefab_new.py
@@ -1,3 +1,9 @@
 class Create():
 	def __init__(self, vmf_file, prefab_name, prefab_text, prefab_icon, workshop_export,indexLine,index):
-		
+		#vmf_file | string | contains the filepath of the vmf file of the prefab
+		#prefab_name | string | is the filename of the prefab file being created
+		#prefab_text | string | is the name of the prefab as it will appear in the main application window
+		#prefab_icon | string | is the filepath of the icon of the prefab as it will appear in the main application window
+		#workshop_export | boolean | that determines whether the prefab will be zipped for export to the workshop
+		#indexLine |  | I have no fucking idea
+		#index |  | I have no idea about this either
