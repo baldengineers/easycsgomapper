cbd189fded5c69f18bd39f3de973b4ce471207c5
Jony <liu.z.jonathan@gmail.com>
2016-09-07 12:57:11 -0700

    Update convert_storage.py

--- convert_storage.py
+++ convert_storage.py
@@ -1,20 +1,15 @@
 import pickle
 import os
-all_list,cur=[[]],0
+all_list = [[]]
+cur = 0 #current tab
 for pfb_file in ["prefab_list.txt","prefab_icon_list.txt","prefab_text_list.txt"]:
-    cfile= open("tf2/prefab_template/"+pfb_file,"r")
-    all_list.append(cfile.readlines())
-    count = len(cfile.readlines())
-    cfile.close()
-for index in all_list[1]:
-    if index == "\n":
-        cur+=0
+    with open("tf2/prefab_template/"+pfb_file,"r") as cfile:
+        all_list.append(cfile.read().splitlines())
+for item in all_list[1]:
+    if item == "\n":
+        cur+=1
     else:
-        all_list[3].append(cur)
-for i in range(cur):
-    for b in range(1,4):
-        del all_list[b][all_list[b].index("\n")]
+        all_list[3].append(cur) #appends the tab that the prefab is in
 
-prefab = open("tf2/prefabs/pfinfo.ezmd","wb")
-pickle.dump(all_list,prefab)
-prefab.close()
+with open("tf2/prefabs/pfinfo.ezmd","wb") as file:
+    pickle.dump(all_list,file)
