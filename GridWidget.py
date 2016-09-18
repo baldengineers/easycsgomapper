4aae465cef35a25ec9d84e96d9ad367f43e9c77e
Jony <liu.z.jonathan@gmail.com>
2016-09-17 22:14:14 -0700

    some nice new updates

--- GridWidget.py
+++ GridWidget.py
@@ -8,31 +8,40 @@ class GridWidget(QWidget):
         super(GridWidget, self).__init__()
         self.spacing = spacing
         self.pList = []#list of points where gridlines intersect
+        self.prefabs = [] #contains list of the prefabs in the grid, contains [icon,coordinate index for the point it is at(top left),moduleName(implement in main program)]
         self.setCursor(Qt.CrossCursor)
-        self.SetAcceptDrops(True)
+        self.setAcceptDrops(True)
         #vars for rubber band
         self.rubberBand = QRubberBand(QRubberBand.Rectangle, self)
         self.origin = QPoint()
         
-    def dragEnterEvent(self, e:)
+    def dragEnterEvent(self, e):
         #http://www.pythonstudio.us/pyqt-programming/drag-and-drop.html
-        print(e.mimeData().formats()) #for testing purposes
         
         if e.mimeData().hasImage:
             e.setDropAction(Qt.CopyAction)
             e.accept()
         else:
             e.ignore()
-            
+
+    def dragMoveEvent(self, e):
+        pass
+    
     def dropEvent(self, e):
         #e.mimeData().imageData
         #multiply the image size by 32/self.spacing
-        
+        for p in self.pList:
+            if e.pos().x() < p.x() and e.pos().y() < p.y():
+                coor_ind = self.pList.index(QPointF(p.x() - self.spacing, p.y() - self.spacing))
+                break
+
+        self.prefabs.append([e.mimeData().imageData(), coor])
+        print(e.mimeData().imageData())
 
     def mousePressEvent(self, e):
 
         #rubber band
-        if e.button() == Qt.LeftButton:
+        if e.button() == Qt.RightButton:
         
             self.origin = QPoint(e.pos())
             self.rubberBand.setGeometry(QRect(self.origin, QSize()))
@@ -47,9 +56,23 @@ class GridWidget(QWidget):
     def mouseReleaseEvent(self, e):
 
         #rubber band
-        if e.button() == Qt.LeftButton:
+        if e.button() == Qt.RightButton:
             print(QRect(self.origin, e.pos()))
             self.rubberBand.hide()
+
+    def wheelEvent(self, e):
+        if self.spacing <= 50 and self.spacing >= 5:
+            e.accept
+            self.changeSpacing(e.delta()/20) #replace with scrollspeed constant
+        else:
+            e.ignore()
+
+        if self.spacing > 50:
+            self.spacing = 50
+        elif self.spacing < 10:
+            self.spacing = 10
+
+        self.repaint()
         
     def paintEvent(self, e):
         qp = QPainter()
@@ -66,22 +89,30 @@ class GridWidget(QWidget):
         x, y = 0, 0
         qp.setPen(Qt.blue)
             
-        self.pList = []
-        for x in range(w/self.spacing):
+        coors = []
+        for x in range(int(w/self.spacing)+1):
             x_ind = x
             x *= self.spacing
             line = QLineF(x,0.0,x,h)
             qp.drawLine(line)
-            self.plist.append([x])
             
-            for y in range(h/self.spacing):
+            for y in range(int(h/self.spacing)+1):
                 y *= self.spacing
                 line = QLineF(x,y,x+self.spacing,y)
                 qp.drawLine(line)
-                self.plist[x_ind].append(y)
+                coors.append([x,y])
+
+        self.pList = []
+        for c in coors:
+            p = QPointF(c[0],c[1])
+            self.pList.append(p)
+        #print(self.pList)
 
     def setSpacing(self, spacing):
         self.spacing = spacing
+
+    def changeSpacing(self, spacing):
+        self.spacing += spacing
 
 def main():
     app = QApplication(sys.argv)
