1a9e13e70e69e0e4538af39b56a3aede905578f9
toads <bridges.anson@gmail.com>
2016-10-09 01:24:48 -0700

    it works
    but on my machine the window only updates when i drag it
    i dont know why

--- cpp_exe/viewer/Debug/vmfScript.py
+++ cpp_exe/viewer/Debug/vmfScript.py
@@ -1,12 +1,19 @@
+from math import *
 def toVert(string_vert):
 	return (int(string_vert.split(" ")[0]),int(string_vert.split(" ")[1]),int(string_vert.split(" ")[2]))
 	
 def crossProduct(vecta, vectb):	
 	#similar to det
 	#ref: wikipedia cross product
-	return ( vecta[1]*vectb[2] - vecta[2]*vectb[1], vecta[2]*vectb[1] - vecta[1]*vectb[2], vecta[0]*vectb[1] - vecta[1]*vectb[0] )
+	return ( vecta[1]*vectb[2] - vecta[2]*vectb[1],  -1*(vecta[0]*vectb[2] - vecta[2]*vectb[0]),vecta[0]*vectb[1] - vecta[1]*vectb[0] )
 	
 	#Vect(y*v.getVectZ() - z*v.getVectY(), z*v.getVectY() - x*v.getVectZ(), x*v.getVectY() - y*v.getVectX());
+
+
+	
+def dotProduct(vecta, vectb):
+	return vecta[0]*vectb[0] + vecta[1]*vectb[1] + vecta[2]*vectb[2] 
+	#x*v.getVectX() + y*v.getVectY() + z*v.getVectZ()
 	
 def coplanar(planePts, point):
 	#eqn of plane.
@@ -14,57 +21,106 @@ def coplanar(planePts, point):
 	ab = (planePts[1][0]-planePts[0][0], planePts[1][1]-planePts[0][1], planePts[1][2]-planePts[0][2])
 	ac = (planePts[2][0]-planePts[0][0], planePts[2][1]-planePts[0][1], planePts[2][2]-planePts[0][2])
 	
-	print("\n ab vect:"+str(ab)+"\n")
-	print("\n ac vect:"+str(ac)+"\n")
+	#print("\n ab vect:"+str(ab)+"\n")
+	#print("\n ac vect:"+str(ac)+"\n")
 	
 	normal = crossProduct(ab, ac)
 	
 	print("\n normal:"+str(normal)+"\n")
 	
-	dval = -normal[0]*planePts[1][0] -normal[1]*planePts[1][1] -normal[2]*planePts[1][2]
+	dval = (-normal[0]*planePts[1][0]) + (-normal[1]*planePts[1][1]) + (-normal[2]*planePts[1][2])
+	
+	#print("\n dval:"+str(dval)+"\n")
 	
 	if normal[0]*point[0] + normal[1]*point[1] + normal[2]*point[2] + dval == 0:
 		if point != planePts[0] and point != planePts[1] and point != planePts[2]:
-			print(str(point)+"\n is coplanar with \n"+str(planePts)+"\n")
+			#print(str(point)+"\n is coplanar with \n"+str(planePts)+"\n")
 			return True
 		else:
 			return False
 	else:
 		return False
+		
+def findCenter(pointList):
+	xtotal,ytotal,ztotal = 0,0,0
+	for point in pointList:
+		xtotal += point[0]
+		ytotal += point[1]
+		ztotal += point[2]
+	return (xtotal/len(pointList), ytotal/len(pointList), ztotal/len(pointList))
+
+	
 	
 def loadVMF(filename):
 	vertlist = []
 	fileinfo = ''
 	planelist = []
+	curnum = -1
+	ent_list = []
 	with open(filename, 'r') as f:
 		vmfDataList = f.readlines()
+	
 	for line in vmfDataList:
 		line = line.strip()
+		if "solid" in line:
+			planelist.append([])
+			vertlist.append([])
+			curnum += 1
 		if "plane" in line:
 			string = line.split('\" \"')[1].replace('"','').replace("(", " ").replace(")", " ")[1:-1]
-			planelist.append(string.split("   "))
+			planelist[curnum].append(string.split("   "))
 			for i in string.split("   "):
-				vertlist.append(i)
+				vertlist[curnum].append(i)
+		if "origin" in line:
+			string = line.split('\" \"')[1].replace('"','').replace("\n","")
+			ent_list.append(string)
+	#print(vertlist)
+	for ind, planelist_in in enumerate(planelist):	
+		for index,plane in enumerate(planelist_in):
+			a = toVert(plane[0])
+			b = toVert(plane[1])
+			c = toVert(plane[2])
+			inNormal = crossProduct((b[0]-a[0], b[1]-a[1],b[2]-a[2]),
+									(c[0]-a[0], c[1]-a[1], c[2]-a[2]))
+			numplane = [a,b,c]
+			newlist = [a]
+			for verti in vertlist:
+				for vert in verti:
+					vert_num = toVert(vert)
+					if vert_num not in numplane:
+						if vert in vertlist[ind]:
+							if coplanar((a,b,c),vert_num):
+								#plane.append(vert)
+								numplane.append(vert_num)
 			
-	for index,plane in enumerate(planelist):
-		a = toVert(plane[0])
-		b = toVert(plane[1])
-		c = toVert(plane[2])
-		for vert in vertlist:
-			vert_num = toVert(vert)
-			if coplanar((a,b,c),vert_num) and vert not in plane:
-				plane.append(vert)
-		
+			center = findCenter(numplane)
+			for vertex in range(1,len(numplane)):
+				if dotProduct(inNormal,crossProduct((numplane[vertex-1][0]-center[0],numplane[vertex-1][1]-center[1],numplane[vertex-1][2]-center[2]),(numplane[vertex][0]-center[0],numplane[vertex][1]-center[1],numplane[vertex][2]-center[2]))) < 0:
+					newlist.append(numplane[vertex])
+					print("neg")
+				else:
+					print("pos")
+					newlist.insert(vertex-1,numplane[vertex])
+			planelist[ind][index] = newlist
 	
 	with open('vertfile.vf','w') as file:
-		for index,i in enumerate(planelist):
-			if index != 0:
+		for i in planelist:
+			for index,p in enumerate(i):
 				fileinfo += "\n"
-			for g in i:
-				fileinfo += g+"\n"
-		file.write(fileinfo)   
+				for g in p:
+					fileinfo += str(g).replace("(","").replace(")","").replace(",","")+"\n"
+		file.write(fileinfo[1:-1])   
+	fileinfo = ''
+	with open('origfile.vf','w') as file:
+		for orig in ent_list:
+			fileinfo += orig+"\n"
+		file.write(fileinfo)
 
 
 
-side_list = loadVMF("vmfs/pentagon.vmf")
+side_list = loadVMF("vmfs/ent.vmf")
+#print(str(coplanar(((64, -64, 384),(64, -64, 64),(64, -192, 384)),(64, -192, 64))))
 
+#64 -64 384
+#64 -64 64
+#64 -192 384
--- cpp_exe/viewer/viewer/main.cpp
+++ cpp_exe/viewer/viewer/main.cpp
@@ -1,3 +1,5 @@
+#define _CRT_SECURE_NO_WARNINGS
+
 #include <stdlib.h>
 #include <stdio.h>
 
@@ -9,6 +11,7 @@
 #include <iostream>
 #include <math.h>
 
+
 #include <gl/glut.h>
 
 #include "Point.h"
@@ -16,10 +19,64 @@
 using namespace std;
 
 
-vector<Point> pointSet;
-vector<bool>  trueSet;
+vector<Point>  pointSet;
+vector<bool>   trueSet;
+vector<string> lines;
+vector<Point>  entSet;
 
-float ypos = -5.0f;
+float ypos = 5.0;
+float t;
+
+GLfloat qaBlack[] = { 0.0, 0.0, 0.0, 1.0 };
+GLfloat qaGreen[] = { 0.0, 1.0, 0.0, 1.0 };
+GLfloat qaRed[] = { 1.0, 0.0, 0.0, 1.0 };
+GLfloat qaBlue[] = { 0.0, 0.0, 1.0, 1.0 };
+GLfloat qaWhite[] = { 1.0, 1.0, 1.0, 1.0 };
+GLfloat qaLowAmbient[] = { 0.05, 0.05, 0.05, 1.0 };
+GLfloat qaFullAmbient[] = { 1.0, 1.0, 1.0, 1.0 };
+
+void drawCube(Point center){
+	glMaterialfv(GL_FRONT, GL_AMBIENT, qaRed);
+
+	for (int i = -1; i < 2; i += 2){
+		glBegin(GL_POLYGON);
+		glVertex3f(center.getPointX() + i*0.1, center.getPointY() + i*0.1, center.getPointZ() + i*0.1);
+		glVertex3f(center.getPointX() + i*0.1, center.getPointY() - i*0.1, center.getPointZ() + i*0.1);
+		glVertex3f(center.getPointX() - i*0.1, center.getPointY() - i*0.1, center.getPointZ() + i*0.1);
+		glVertex3f(center.getPointX() - i*0.1, center.getPointY() + i*0.1, center.getPointZ() + i*0.1);
+		glEnd();
+
+		glBegin(GL_POLYGON);
+		glVertex3f(center.getPointX() + i*0.1, center.getPointY() + i*0.1, center.getPointZ() + i*0.1);
+		glVertex3f(center.getPointX() + i*0.1, center.getPointY() - i*0.1, center.getPointZ() + i*0.1);
+		glVertex3f(center.getPointX() + i*0.1, center.getPointY() - i*0.1, center.getPointZ() - i*0.1);
+		glVertex3f(center.getPointX() + i*0.1, center.getPointY() + i* 0.1, center.getPointZ() - i*0.1);
+		glEnd();
+
+		glBegin(GL_POLYGON);
+		glVertex3f(center.getPointX() + i*0.1, center.getPointY() + i*0.1, center.getPointZ() + i*0.1);
+		glVertex3f(center.getPointX() - i*0.1, center.getPointY() + i*0.1, center.getPointZ() + i*0.1);
+		glVertex3f(center.getPointX() - i*0.1, center.getPointY() + i*0.1, center.getPointZ() - i*0.1);
+		glVertex3f(center.getPointX() + i*0.1, center.getPointY() + i*0.1, center.getPointZ() - i*0.1);
+		glEnd();
+	}
+}
+
+void loadEntPoints(const char *filename){
+	ifstream input(filename);
+	string current_line;
+
+	while (getline(input, current_line)){
+		if (current_line != ""){
+			int firstSpace = current_line.find_first_of(" ");
+			int lastSpace = current_line.find_last_of(" ");
+			//z and y values swapped because source engine is bonkers!!
+
+			entSet.push_back(Point(stod(current_line.substr(0, firstSpace)) / 64, stod(current_line.substr(lastSpace, current_line.length() - 1)) / 64, stod(current_line.substr(firstSpace, lastSpace)) / 64));
+
+		}
+	}
+}
 
 void loadPoints(const char *filename){
 	ifstream input(filename);
@@ -34,52 +91,80 @@ void loadPoints(const char *filename){
 
 			pointSet.push_back(Point(stod(current_line.substr(0, firstSpace)) / 64, stod(current_line.substr(lastSpace, current_line.length() - 1)) / 64, stod(current_line.substr(firstSpace, lastSpace)) / 64));
 			trueSet.push_back(false);
+			lines.push_back(current_line + "\n");
 			//printf("not newline");
+
 		}
 		else{
-			printf("newline");
+			//printf("newline");
 			trueSet.push_back(true);
+			pointSet.push_back(Point(0,0,0));
+
+			lines.push_back(current_line + "\n");
+
+
 		}
 	}
 }
+
 void parseVMF(void){
 	system("python vmfScript.py");
 }
 
+
 void renderPoints(void){
-	
-	//gluLookAt(0.0, ypos, 0.0, 4.0, 0.0, -4.0, 0.0, 1.0, 0.0);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glEnable(GL_DEPTH_TEST);
+
+	glMaterialfv(GL_FRONT, GL_SPECULAR, qaWhite);
+	glLightfv(GL_LIGHT0, GL_AMBIENT, qaLowAmbient);
+
+	glMaterialfv(GL_FRONT, GL_AMBIENT, qaWhite);
+	glMaterialfv(GL_FRONT, GL_DIFFUSE, qaWhite);
+	glMaterialf(GL_FRONT, GL_SHININESS, 128.0);
+	glLightfv(GL_LIGHT0, GL_AMBIENT, qaLowAmbient);
 	glLoadIdentity();
-	gluLookAt(0.0f, ypos, 0.0f,
+	
+	glShadeModel(GL_FLAT);
+	gluLookAt(6 * cos(t) + 4, ypos, -6 * sin(t) - 4,
 		4.0f, 0.0f, -4.0f,
 		0.0f, 1.0f, 0.0f);
-	
-	
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	//render solids
 	glBegin(GL_POLYGON);
+	glMaterialfv(GL_FRONT, GL_SPECULAR, qaWhite);
 	for (int ind = 0; ind < pointSet.size(); ind++){
 		if (trueSet[ind] == false){
 			glVertex3f(pointSet[ind].getPointX(), pointSet[ind].getPointY(), pointSet[ind].getPointZ());
+			//printf(lines[ind].c_str());
 		}
 		else{
+			//printf(lines[ind].c_str());
 			glEnd();
 			glBegin(GL_POLYGON);
 		}
 	}
-	
-	
 	glEnd();
+
+	//render ent representing cubes
+
+
+	for (int cur = 0; cur < entSet.size(); cur++){
+		drawCube(entSet[cur]);
+	}
+	glLightfv(GL_LIGHT0, GL_AMBIENT, qaFullAmbient);
 	glTranslatef(0.0, ypos, 0);
 	glutSwapBuffers();
 	glutPostRedisplay();
-	
-	ypos += 0.1f;
+
+
+	t += 0.05;
+	//ypos += 0.1f;
 	Sleep(100);
-
-
 	
-
 }
+
+
 
 void changeSize(int w, int h) {
 
@@ -112,23 +197,44 @@ int main(int argc, char **argv){
 	glutInitWindowPosition(100, 100);
 	glutInitWindowSize(500, 500);
 	glutCreateWindow("pfb render");
-
+	glClearColor(0.0, 0.0, 0.0, 0.0);
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
 	glViewport(0, 0, 500, 500);
 	gluPerspective(90.0f, 1, 0.1f, 100.0f);
 
+	glLightModeli(GL_LIGHT_MODEL_LOCAL_VIEWER, GL_TRUE);
+	glEnable(GL_LIGHTING);
+	glEnable(GL_LIGHT0);
+
+	GLfloat qaAmbientLight[] = { 0.05, 0.05, 0.05, 1.0 };
+	GLfloat qaDiffuseLight[] = { 0.8, 0.8, 0.8, 1.0 };
+	GLfloat qaSpecularLight[] = { 1.0, 1.0, 1.0, 1.0 };
+	GLfloat qaLightPosition[] = { 4.0, 12.0, -4.0, 1.0 };
+
+	glLightfv(GL_LIGHT0, GL_AMBIENT, qaAmbientLight);
+	glLightfv(GL_LIGHT0, GL_DIFFUSE, qaDiffuseLight);
+	glLightfv(GL_LIGHT0, GL_SPECULAR, qaSpecularLight);
+	glLightfv(GL_LIGHT0, GL_POSITION, qaLightPosition);
+
+	
+
 	glMatrixMode(GL_MODELVIEW);
 	
 	parseVMF();
 	loadPoints("vertfile.vf");
-	//glTranslatef(0.0, -.5, 0.0);
-	//glRotatef(90, 1, 0, 0.1);
+	loadEntPoints("origfile.vf");
+	glTranslatef(10.0, 5.0, -10.0);
+	glRotatef(135, 1, 0, 0.1);
 	glutDisplayFunc(renderPoints);
 	glutReshapeFunc(changeSize);
+
 	glutIdleFunc(renderPoints);
+	
 
 	glutMainLoop();
 
-	return 1;
+	
+
+	return 0;
 }
