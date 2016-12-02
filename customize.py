203b4199d0447c794c0f00aafcfca813a0fc9be2
toads <bridges.anson@gmail.com>
2016-12-01 17:26:24 -0800

    somewhat acceptable vpk dir shit

--- customize.py
+++ customize.py
@@ -4,24 +4,64 @@ import vpk
 class makeFileTree():
     def __init__(self,path_list):
         self.path_list = path_list
-        self.slashct = 0
+        self.dirct = 0
+
+        self.tree = []
+        self.acclist = []
 
         #determine largest # of directories (1 too high because last one is file?)
-        for path in path_list:
-            if path.count("/") > self.slashct:
-                self.slashct = path.count("/")
-                
-        #organisation:
-        #main dir is list containing number of subdirs
-        # eg. if there's a system like ./a/ and ./b/
-        #                  ./a/1/ and ./a/2/     ./b/1 and ./b/2/
-        # the list looks like[ [#a [#1], [#2]], [#b [#1], [#2]] ]
-        #each "dir" or list can contain lists (subdirs) or strings (paths)
-
-        #each dir's real name (eg. "materials/kkk_posters/") needs to be assigned
-        #to a set (tuple) of values corresponding to the dir's index in the list.
-        #this will be done via a dictionary
+        for path in self.path_list:
+            if path.count("/") > self.dirct:
+                self.dirct = path.count("/")   
+        for dirlvl in range(self.dirct):
+            self.tree.append([])
+            self.acclist.append([])
+            for path in self.path_list:
+                try:
+                    current = path.split("/")[dirlvl]
+                    before = '' if dirlvl == 0 else path.split("/")[dirlvl-1]
+                    bf = '' if dirlvl < 2 else path.split("/")[dirlvl-2]
+                    if current not in self.acclist[dirlvl]:
+                        if "." in current:
+                            self.acclist[dirlvl].append(current)
+                            self.tree[dirlvl].append((current,before,path,dirlvl))
+                    if before not in self.acclist[dirlvl-1] and dirlvl != 0:
+                        self.tree[dirlvl-1].append((before,bf,"/".join(path.split("/")[0:dirlvl]),dirlvl-1))
+                        self.acclist[dirlvl-1].append(before)      
+                except:
+                    pass
     
+    def getOtherFilesInDirectory(self, path):
+        #returns a tuple containing 2 tuples (1 for files, 1 for dirs)
+        self.currentOtherFilesInDir = ( [],[] )
+        dirf = 0
+        directory = 0
+        parent = ''
+        #find item tuple in tree
+        for dirlvl in self.tree:
+            for item in dirlvl:
+                if path == item[2] and not dirf:
+                    parent = item[1]
+                    directory = item[3]
+                    dirf = 1
+                    print("dir found: "+path)
+        
+        for item in self.tree[directory]:
+            if item[1] == parent:
+                #if in same dir
+                if "." in item[0]:
+                    #is file
+                    self.currentOtherFilesInDir[0].append(item[0])
+                else:
+                    #is dir
+                    self.currentOtherFilesInDir[1].append(item[0])
+        #for testing, otherwise should just return the tuple
+        print("Files:")
+        for file in sorted(self.currentOtherFilesInDir[0]):
+            print("-"+file)
+        print("Dirs:")
+        for direc in sorted(self.currentOtherFilesInDir[1]):
+            print("-"+direc)
 
 def loadVMF(filepath):
     with open(filepath, "r") as f:
@@ -57,3 +97,8 @@ def loadVPKfiles(filepath):
         temp_list.append(path)
 
     return temp_list
+
+#for testing
+mat_vpk = makeFileTree(loadVPKfiles("C:/Program Files (x86)/Steam/steamapps/common/Team Fortress 2/tf/tf2_textures_dir.vpk"))
+mat_vpk.getOtherFilesInDirectory("materials/patterns/powerhouse")
+
